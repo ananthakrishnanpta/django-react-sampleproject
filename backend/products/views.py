ea2a3092e221cd989b4bc856c4fe64435ee34bf8
ananthakrishnanpta <ananthakrishnanpta@gmail.com>
2025-05-21 15:12:13 +0530

    first commit

---
+++ backend/products/views.py
@@ -0,0 +1,11 @@
+
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import Product
+from .serializers import ProductSerializer
+
+class ProductListView(APIView):
+    def get(self, request):
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True, context={'request': request})
+        return Response(serializer.data)
