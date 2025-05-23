d2d486f065e5684de32b20920e5bbe44fa0a202f
ananthakrishnanpta <ananthakrishnanpta@gmail.com>
2025-05-23 15:51:41 +0530

    edits

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -120,8 +120,6 @@ USE_TZ = True
 
 
 
-
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -7,7 +7,7 @@ const ProductCard = ({product}) => {
     return (
         <div className="col-md-4">
             <div className="card">
-                <img src="{product.image}" alt={product.name} className="card-img-top" />
+                <img src={product.image} alt={product.name} className="card-img-top" />
                 <div className="card-body">
                     <h5 className="card-title">
                         Title : {product.name}
